c944d0e1848e29b752d1aef126cd8c69ef1639a1
Ducky <masterless123@gmail.com>
2021-03-27 15:06:11 -0400

    Modify .gitignore to reflect modifications

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(visual-fill-column ledger-mode flycheck dashboard company lsp-jedi lsp-java yasnippet dap-mode lsp-treemacs lsp-ivy lsp-ui lsp-mode org-superstar org-download org-cliplink org-capture org-agenda doom-themes use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
