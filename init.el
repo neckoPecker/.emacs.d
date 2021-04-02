ecd189a7d20e6546757f24561721e596be29f60d
Ducky <masterless123@gmail.com>
2021-04-01 23:07:15 -0400

    Improve appearance

--- init.el
+++ init.el
@@ -6,7 +6,6 @@
 ;; 
 
 ;;; Code:
-
 ;;;; Package Initilization
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -28,12 +27,15 @@
 	      fill-column 80		                  ; Width for automatic line breaks
 	      help-window-select t	                  ; You asked for help, so we'll give to you™!
 	      initial-scratch-message ""              ; Empty *scratch* buffer
+		  line-spacing 2						  ; Things look less cluttered
 	      recenter-positions '(5 top bottom)      ; Modify recentering positions
 	      scroll-conservatively 101               ; Add margin for scrolling vertically
 	      tab-width 4                             ; I dare you set this to 2. I double dare you.
 	      uniquify-buffer-name-style 'forward     ; Uniquify buffer names
 	      warning-minimum-level                   ; "A warning? I'll just pretend it's not there..."
-	      x-stretch-cursor)                       ; Stretch cursor to glyph width
+	      x-stretch-cursor                        ; Stretch cursor to glyph width
+		  x-underline-at-descent-line t			  ; Makes underlines look better
+		  )
 
 (blink-cursor-mode 1)			; "Where the hell is my cur...oh there it is"
 (electric-pair-mode 1)			; "You complete me..."
@@ -46,7 +48,102 @@
 (tool-bar-mode -1)				; *ew* Ugly Tool Bar
 
 ;;;; Styling
-(set-frame-font "Roboto Mono Light 12" nil t) ; Font
+;;;;; Font
+(set-frame-font "Roboto Mono Light 12" nil t)
+;;;;; Theme
+(use-package doom-themes
+  :ensure t
+  :config (load-theme 'doom-plain t))
+
+;;;;; Window
+;; Straight up copied from Rougier's Elegant-Emacs
+;; https://github.com/rougier/elegant-emacs
+
+(fringe-mode '(0 . 0))
+
+(set-frame-parameter (selected-frame)
+					 'internal-border-width 24)
+
+(setq window-divider-default-right-width 3)
+(setq window-divider-default-places 'right-only)
+(window-divider-mode)
+
+(set-fontset-font "fontset-default"  '(#x2600 . #x26ff) "Fira Code 16")
+
+(define-key mode-line-major-mode-keymap [header-line]
+  (lookup-key mode-line-major-mode-keymap [mode-line]))
+
+(defun mode-line-render (left right)
+  (let* ((available-width (- (window-width) (length left) )))
+    (format (format "%%s %%%ds" available-width) left right)))
+(setq-default mode-line-format
+     '((:eval
+       (mode-line-render
+       (format-mode-line (list
+         (propertize "☰" 'face `(:inherit mode-line-buffer-id)
+                         'help-echo "Mode(s) menu"
+                         'mouse-face 'mode-line-highlight
+                         'local-map   mode-line-major-mode-keymap)
+         " %b "
+         (if (and buffer-file-name (buffer-modified-p))
+             (propertize "(modified)" 'face `(:inherit face-faded)))))
+       (format-mode-line
+        (propertize "%4l:%2c  " 'face `(:inherit face-faded)))))))
+
+(defun set-modeline-faces ()
+
+  ;; Mode line at top
+  (set-face 'header-line                                 'face-strong)
+  (set-face-attribute 'header-line nil
+                                :underline (face-foreground 'default))
+  (set-face-attribute 'mode-line nil
+                      :height 10
+                      :underline (face-foreground 'default)
+                      :overline nil
+                      :box nil 
+                      :foreground (face-background 'default)
+                      :background (face-background 'default))
+  (set-face 'mode-line-inactive                            'mode-line)
+  
+  ;; Mode line at bottom
+  ;; (set-face 'header-line                                 'face-strong)
+  ;; (set-face-attribute 'mode-line nil
+  ;;                     :height 1.0
+  ;;                     :overline (face-background 'default)
+  ;;                     :underline nil
+  ;;                     :foreground (face-foreground 'default)
+  ;;                     :background (face-background 'face-subtle)
+  ;;                     :box `(:line-width 2
+  ;;                            :color ,(face-background 'face-subtle)
+  ;;                            :style nil))
+  ;; (set-face 'mode-line-highlight '(face-popout mode-line))
+  ;; (set-face 'mode-line-emphasis  'face-strong)
+  ;; (set-face-attribute 'mode-line-buffer-id nil :weight 'regular)
+  ;; (set-face-attribute 'mode-line-inactive nil
+  ;;                     :height 1.0
+  ;;                     :overline (face-background 'default)
+  ;;                     :underline nil
+  ;;                     :foreground (face-foreground 'face-faded)
+  ;;                     :background (face-background 'face-subtle)
+  ;;                     :box `(:line-width 2
+  ;;                            :color ,(face-background 'face-subtle)
+  ;;                            :style nil))
+
+
+  (set-face-attribute 'cursor nil
+                      :background (face-foreground 'default))
+  (set-face-attribute 'window-divider nil
+                      :foreground (face-background 'mode-line))
+  (set-face-attribute 'window-divider-first-pixel nil
+                      :foreground (face-background 'default))
+  (set-face-attribute 'window-divider-last-pixel nil
+                      :foreground (face-background 'default))
+  )
+
+;; Comment if you want to keep the modeline at the bottom
+(setq-default header-line-format mode-line-format)
+(setq-default mode-line-format'(""))
+
 ;;;; Mode Packages
 ;;;;; Markdown Mode
 (use-package markdown-mode
