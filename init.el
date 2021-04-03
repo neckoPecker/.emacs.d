cb9eb53301d450df754e6570f9949d8e7175e9e1
neckoPecker <masterless123@gmail.com>
2021-04-03 00:37:04 -0400

    Install org-roam-server

--- init.el
+++ init.el
@@ -208,6 +208,20 @@
 		 ("C-c n i" . org-roam-insert)
 		 ("C-c n I" . org-rooam-insert-immediate)))
 
+;;;;;; Roam Server
+(use-package org-roam-server
+  :ensure t
+  :config (setq org-roam-server-host "127.0.0.1"
+				org-roam-server-port 8080
+				org-roam-server-authenticate nil
+				org-roam-server-export-inline-images t
+				org-roam-server-serve-files nil
+				org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+				org-roam-server-network-poll t
+				org-roam-server-network-arrows nil
+				org-roam-server-network-label-truncate t
+				org-roam-server-network-label-truncate-length 60
+				org-roam-server-network-label-wrap-length 20))
 ;;;;; Outshine Mode
 (use-package outshine
   :ensure t
