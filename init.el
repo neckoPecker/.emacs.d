ffebfa61d8b0d89f4eb9c9f6850f783052ae7c41
neckoPecker <masterless123@gmail.com>
2021-04-22 18:50:24 -0400

    Fix formatting
    - Fixed weird formatting of comments
    - Changed tab-width to tab-stop-list, which actually changes tab size

--- init.el
+++ init.el
@@ -20,7 +20,6 @@
                 use-package-always-ensure t))
 
 ;;;;; Move customization options out of init.el
-
 (unless (file-exists-p (expand-file-name "custom.el" user-emacs-directory))
   (write-region "" nil (expand-file-name "custom.el" user-emacs-directory)))
 
@@ -28,34 +27,35 @@
 (load custom-file)
 
 ;;;; Insane Defaults
-(setq-default delete-by-moving-to-trash t         ; Move files to trash. Don't delete!
-	      fill-column 80		                  ; Width for automatic line breaks
-	      help-window-select t	                  ; You asked for help, so we'll give to you™!
-		  inhibit-startup-screen t				  ; Disable annyoing startup
-	      initial-scratch-message ""              ; Empty *scratch* buffer
-		  line-spacing 2						  ; Things look less cluttered
-	      recenter-positions '(5 top bottom)      ; Modify recentering positions
-	      scroll-conservatively 101               ; Add margin for scrolling vertically
-	      tab-width 4                             ; I dare you set this to 2. I double dare you.
-	      uniquify-buffer-name-style 'forward     ; Uniquify buffer names
-	      warning-minimum-level                   ; "A warning? I'll just pretend it's not there..."
-	      x-stretch-cursor                        ; Stretch cursor to glyph width
-		  x-underline-at-descent-line t			  ; Makes underlines look better
-		  )
+(setq-default delete-by-moving-to-trash t 			; move files to trash. Don't delete!
+			  fill-column 80 			; Width for automatic line breaks
+			  help-window-select t 			; You asked for help, so we'll give to you™!
+			  inhibit-startup-screen t		; Disable annyoing startup
+			  initial-scratch-message "" 		; Empty *scratch* buffer
+			  line-spacing 2 			; Things look less cluttered
+			  recenter-positions '(5 top bottom) 	; Modify recentering positions
+			  scroll-conservatively 101 		; Add margin for scrolling vertically
+			  tab-stop-list 4	 		; I dare you set this to 2. I double dare you.
+			  uniquify-buffer-name-style 'forward 	; Uniquify buffer names
+			  warning-minimum-level 		; "A warning? I'll just pretend it's not there..."
+			  x-stretch-cursor			; Stretch cursor to glyph width
+			  x-underline-at-descent-line t 	; Makes underlines look better
+			  )
 
-(blink-cursor-mode 1)			; "Where the hell is my cur...oh there it is"
-(electric-pair-mode 1)			; "You complete me..."
-(delete-selection-mode 1)		; Replace region when inserting text
+(blink-cursor-mode 1)		; "Where the hell is my cur...oh there it is"
+(electric-pair-mode 1)		; "You complete me..."
+(delete-selection-mode 1)	; Replace region when inserting text
 (fset 'yes-or-no-p 'y-or-n-p)	; Typing yes/no is very annoying
-(global-subword-mode 1)			; Iterate through CamelCase words
-(global-auto-revert-mode 1)		; Keep buffers up to date
-(menu-bar-mode -1)				; *ew* Ugly Menu Bar
-(toggle-scroll-bar -1) 			; *ew* Ugly Scroll Bar
-(tool-bar-mode -1)				; *ew* Ugly Tool Bar
+(global-subword-mode 1)		; Iterate through CamelCase words
+(global-auto-revert-mode 1)	; Keep buffers up to date
+(menu-bar-mode -1)		; *ew* Ugly Menu Bar
+(toggle-scroll-bar -1) 		; *ew* Ugly Scroll Bar
+(tool-bar-mode -1)		; *ew* Ugly Tool Bar
 
 ;;;; Styling
 ;;;;; Font
 (set-frame-font "Roboto Mono Light 12" nil t)
+
 ;;;;; Theme
 (use-package doom-themes
   :ensure t
@@ -139,8 +139,7 @@
 ;;;;; CMake Mode
 (use-package cmake-mode
   :ensure t
-  :config (add-to-list 'auto-mode-alist '("/CMakeLists.txt\\'" . cmake-mode) t)
-  )
+  :config (add-to-list 'auto-mode-alist '("/CMakeLists.txt\\'" . cmake-mode) t))
 
 ;;;;; Markdown Mode
 (use-package markdown-mode
@@ -162,14 +161,15 @@
 ;;;;; Lua Mode
 (use-package lua-mode
   :ensure t)
+
 ;;;;; Org Mode
 ;;;;;; General
 (use-package org
-  :config (setq org-hide-emphasis-markers t ; Org headings look much more cleaner
-				org-log-done 'time			; Show when you finished a task
-				org-log-done 'note			; Prompt a note for when you finish a task
-				org-startup-indented t)		; View org document as indented
-  (plist-put org-format-latex-options :scale 1.5)) ; Increase the size of latex expressions/equations
+  :config (setq org-hide-emphasis-markers t 		; Org headings look much more cleaner
+				org-log-done 'time 	; Show when you finished a task
+				org-log-done 'note 	; Prompt a note for when you finish a task
+				org-startup-indented t)	; View org document as indented
+  (plist-put org-format-latex-options :scale 1.5)) 	; Increase the size of latex expressions/equations
 
 ;;;;;; Babel
 (org-babel-do-load-languages
@@ -184,11 +184,12 @@
   :bind ("C-c a" . org-agenda)
   :config (setq org-agenda-span 14
 				org-agenda-todo-ignore-timestamp t
-				org-agenda-prefix-format		    ; Property tags are often too long to the
+				org-agenda-prefix-format	    ; Property tags are often too long to the
 				'((agenda . " %i  %-30:c%?-12t% s") ; point where they misalign the agenda
-				  (todo . " %i %-12:c")				; view.
+				  (todo . " %i %-12:c")		    ; view.
 				  (tags . " %i %-12:c")
 				  (search . " %i %-12:c"))))
+
 ;;;;;; Capture
 (use-package org-capture
   :bind ("C-c c" . org-capture))
@@ -233,6 +234,7 @@
 (use-package outshine
   :ensure t
   :hook (emacs-lisp-mode . outshine-mode))
+
 ;;;; Misc Packages
 ;;;;; Company
 (use-package company
@@ -245,10 +247,10 @@
   :ensure t
   :init (dashboard-setup-startup-hook)
   :config (setq dashboard-items '((recents . 5)
-								  (bookmarks . 5)
-								  (projects . 5)
-								  (agenda . 5)
-								  (registers . 5))))
+				  (bookmarks . 5)
+				  (projects . 5)
+				  (agenda . 5)
+				  (registers . 5))))
 
 ;;;;; Dap
 (use-package dap-mode
@@ -295,8 +297,8 @@
 (use-package lsp-ui
   :ensure t
   :config (setq lsp-ui-sideline-show-diagnostics t
-				lsp-ui-show-hover t
-				lsp-ui-sideline-update-mode t))
+		lsp-ui-show-hover t
+		lsp-ui-sideline-update-mode t))
 
 ;;;;;; Lsp Java
 (use-package lsp-java
@@ -306,6 +308,7 @@
 ;;;;; Magit
 (use-package magit
   :ensure t)
+
 ;;;;; Projectile
 (use-package projectile
   :ensure t
@@ -318,7 +321,7 @@
   :hook ((text-mode . turn-on-visual-line-mode)
          (visual-line-mode . visual-fill-column-mode))
   :config (setq visual-line-mode t
-				visual-fill-column-width 80))
+		visual-fill-column-width 80))
 
 (provide 'init)
 ;;;;; Yasnippet
