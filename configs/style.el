be82a6b570ca5c2db8d3cc885cf0f290179fa217
Ducky <masterless123@gmail.com>
2021-06-07 10:34:08 -0400

    Add padding

--- configs/style.el
+++ configs/style.el
@@ -6,6 +6,17 @@
 
 ;;; Code:
 ;;;; Font
-;; (set-frame-font "Roboto Mono Ligh 12" nil t) ; Optional
+
+(defun init-style ()
+  "Contain styles."
+  (setq left-margin-width 2)		; Left margin
+  (setq right-margin-width 2)		; Right margin
+  (setq header-line-format " ")		; Set margin as just empty
+  (set-face-attribute 'header-line nil	; Top margin height
+		      :height 200)
+  )
+
+(add-hook 'after-init-hook (init-style))
+
 
 ;;; style.el ends here
